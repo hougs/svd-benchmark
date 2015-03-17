abdaf82ffbe85eb3af0d0d3a0d00e22eeca2309d
juliet <juliet@bottou01-10g.pa.cloudera.com>
2015-03-17 12:47:22 -0700

    Use args correctly.

--- scripts/gen-matrices.sh
+++ scripts/gen-matrices.sh
@@ -2,7 +2,8 @@
 
 spark-submit --class com.cloudera.ds.svdbench.GenerateMatrix \
   --master yarn-client --executor-memory 20G --num-executors 50 \
-  --driver-class-path target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-  target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar hdfs://juliet/matrix-svd \
-  yarn-cluster 1000 100 30 100
+  --driver-class-path ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  --path hdfs://juliet/matrix-svd --master yarn-cluster --nRows 1000 \
+  --nCols 100 --nNonZero 30 --blockSize 100
 
