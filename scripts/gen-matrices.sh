85f8473027d672013884de766339e00ce78cddde
Juliet Hougland <juliet@cloudera.com>
2015-03-18 14:24:08 -0700

    Add script for factorizing a matrix with spark.

--- scripts/gen-matrices.sh
+++ scripts/gen-matrices.sh
@@ -1,13 +1,27 @@
 !#/bin/bash
+# This script generates a random matrix with the specified number of rows and columns. Every entry
+# in this matrix is a zero or a one. The elements are specified by drawing from the uniform
+# distribution on (0, 1) for every element in the matrix and setting the value to 1 is the the
+# sample is less than the specified sparisty level or to zero otherwise. This allows us to
+# approximately specify the percentage of nonzero elements.
+#
+# usage: gen-matrices.sh nRows nCols fracNonZero blockSize master
+# nRows and nCols specify the number of rows and columns in the generated matrix. fracNonZero is
+# the approximate fraction of nonzero elements in the generated matrix. blockSize
+
+N_ROWS=$1
+N_COLS=$2
+FRAC_NON=$3
+BLOCK_SIZE=$4
+MASTER=$5
 
 export SPARK_HOME=/home/juliet/bin/spark-1.3.0-bin-hadoop2.4/bin
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 
 $SPARK_HOME/spark-submit --class com.cloudera.ds.svdbench.GenerateMatrix \
   --conf spark.yarn.jar=hdfs:///user/juliet/bin/spark-1.3.0-bin-hadoop2.4/lib/spark-assembly-1.3.0-hadoop2.4.0.jar \
-  --master yarn-client --executor-memory 8G --num-executors 10 \
+  --master $MASTER --executor-memory 8G --num-executors 10 \
   --driver-class-path ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
   ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-  --path hdfs:///user/juliet/matrix-svd --nRows 1000 \
-  --nCols 100 --fracNonZero 0.01 --blockSize 100
-
+  --path hdfs:///user/juliet/matrix-svd --nRows $N_ROWS \
+  --nCols $N_COLS --fracNonZero $FRAC_NON --blockSize $BLOCK_SIZE
---
+++ scripts/spark-svd.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+# This script reads in a file representing a matrix from the specified path, uses spark to compute
+# the SVD (M=U*S*V^{*}) of the matrix, and writes out a file in HDFS for U, and two local files for
+# a vector representation of the singular vectors from S and V.
+#
+# usage: spark-svd.sh inputPath outUPath outSPath outVPath master
+# Where inputPath and outUPath are paths in hdfs, outSPath and outVPath are local paths, and
+# master is a URL for a Spark master.
+
+INPUT_PATH=$1
+OUT_U=$2
+OUT_S=$3
+OUT_V=$4
+MASTER=$5
+
+export SPARK_HOME=/home/juliet/bin/spark-1.3.0-bin-hadoop2.4/bin
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+
+$SPARK_HOME/spark-submit --class com.cloudera.ds.svdbench.SparkSVD \
+  --conf spark.yarn.jar=hdfs:///user/juliet/bin/spark-1.3.0-bin-hadoop2.4/lib/spark-assembly-1.3.0-hadoop2.4.0.jar \
+  --master $MASTER --executor-memory 8G --num-executors 10 \
+  --driver-class-path ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  --inPath $INPUT_PATH --outUPath $OUT_U --outSPath $OUT_S --outVPath $OUT_V
