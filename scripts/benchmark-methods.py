2a61634b03408b5b734ff93a32dec513e3afa2ee
Juliet Hougland <juliet@cloudera.com>
2015-03-19 13:59:48 -0700

    Runnable over a grid of parameters.

--- scripts/benchmark-methods.py
+++ scripts/benchmark-methods.py
@@ -5,7 +5,6 @@
 
 import subprocess as sub
 import csv
-import sys
 
 def time_it(program):
     command="/usr/bin/env time -f \"%E\" " +  program
@@ -54,8 +53,15 @@ def stochastic_factorize_and_time(wd, in_path, out_path, rank):
     return elapsed_time
 
 
-def process_one_param_set(wd, gen_mat_path, n_rows, n_cols, frac, out_u, out_s, out_v, out_lan, lan_rank, stoch_rank,
-                          out_stoch, block_size, master, spark_home, csv_writer):
+def process_one_param_set(wd, n_rows, n_cols, frac, lan_rank, stoch_rank, block_size, master, spark_home, project_home, hdfs_root, csv_writer):
+    # setup paths for these matrices
+    gen_mat_path=hdfs_root + "/gen-matrix-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+    out_u=hdfs_root + "/u-spark-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+    out_s=hdfs_root + "/s-spark-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+    out_v=hdfs_root + "/v-spark-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+    out_lan=hdfs_root + "/lanczos-out-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+    out_stoch=hdfs_root + "/stoch-out-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
+
     generate_matrix(wd, gen_mat_path, n_rows, n_cols, frac, block_size, master, spark_home)
     csv_writer.writerow([spark_factorize_and_time(wd, gen_mat_path, out_u, out_s, out_v, master, spark_home)]
                         + ['spark', n_rows, n_cols, frac, block_size])
@@ -64,18 +70,17 @@ def process_one_param_set(wd, gen_mat_path, n_rows, n_cols, frac, out_u, out_s, 
     csv_writer.writerow([stochastic_factorize_and_time(wd, gen_mat_path, out_stoch, stoch_rank)]
                         + ['stoch', n_rows, n_cols, frac])
 
-def main(argv):
-    gen_matrix_path="path"
-    n_rows=3
-    n_cols=3
-    frac=0.1
+def main():
+    rows = [20000, 40000, 100000]
+    n_cols=10000
+    frac=[0.5, 0.1, 0.01, 0.001]
     block_size=2
-    master="local"
+    master="yarn-client"
     spark_home="/home/juliet/bin/spark-1.3.0-bin-hadoop2.4/bin"
-    u_path="path"
-    s_path="path"
-    v_path="path"
-    out_lan="lanpath"
+    lan_rank=60
+    stoch_rank=20
+    project_root="/home/juliet/src/svd-benchmark"
+    hdfs_root="hdfs:///user/juliet/matrix"
 
     # Setup our env
     wd = sub.check_output("pwd", shell=True).strip()
@@ -84,13 +89,12 @@ def main(argv):
     sub.call(["chmod +x %s/scripts/lanczos-svd.sh" % wd], shell=True)
     sub.call(["chmod +x %s/scripts/stochastic-svd.sh" % wd], shell=True)
 
-
-    # bottou spark home /home/juliet/bin/spark-1.3.0-bin-hadoop2.4/bin
     with open('results/experiment.csv', 'wb') as exp_rez:
         observation_writer = csv.writer(exp_rez, delimiter=",")
-        for n_rows in [10000, 20000, 400000]:
-            process_one_param_set(wd, gen_matrix_path, n_rows, n_cols, frac, u_path, s_path, v_path, out_lan, block_size, master,
-                              spark_home, observation_writer)
+        for n_rows in rows:
+            for sparse_frac in frac:
+                process_one_param_set(wd, n_rows, n_cols, sparse_frac, lan_rank, stoch_rank, block_size, master,
+                              spark_home, project_root, hdfs_root, observation_writer)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main()
