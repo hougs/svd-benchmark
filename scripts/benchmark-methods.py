ba57baaa0f7ab38c4e03dbcdfd85c907fb1fc6e6
Juliet Hougland <juliet@cloudera.com>
2015-03-19 14:14:23 -0700

    Add project home var.

--- scripts/benchmark-methods.py
+++ scripts/benchmark-methods.py
@@ -17,16 +17,16 @@ def time_it(program):
         raise
     return out
 
-def generate_matrix(wd, out_path, n_rows, n_cols, frac, block_size, master, spark_home):
-    gen_mat_args = (wd.strip(), out_path, n_rows, n_cols, frac, block_size, master, spark_home)
+def generate_matrix(project_root, out_path, n_rows, n_cols, frac, block_size, master, spark_home):
+    gen_mat_args = (project_root, out_path, n_rows, n_cols, frac, block_size, master, spark_home)
     gen_mat_cmd = "%s/scripts/gen-matrix.sh %s %s %s %s %s %s %s" % gen_mat_args
     try:
         sub.call(gen_mat_cmd, shell=True)
     except OSError:
         print "Oops! OS Error. Could not run the command:\n %s" % gen_mat_cmd
 
-def spark_factorize_and_time(wd, in_path, out_u, out_s, out_v, master, sparkHome):
-    svd_args = (wd.strip(), in_path, out_u, out_s, out_v, master, sparkHome)
+def spark_factorize_and_time(project_root, in_path, out_u, out_s, out_v, master, sparkHome):
+    svd_args = (project_root, in_path, out_u, out_s, out_v, master, sparkHome)
     svd_cmd = "{%s/scripts/spark-svd.sh %s %s %s %s %s %s 2> spark.logs; }" % svd_args
     try:
         elapsed_time = time_it(svd_cmd)
@@ -34,8 +34,8 @@ def spark_factorize_and_time(wd, in_path, out_u, out_s, out_v, master, sparkHome
         print "Oops! OS Error. Could not run the command:\n %s" % svd_cmd
     return elapsed_time
 
-def lanczos_factorize_and_time(wd, in_path, out_path, n_rows, n_cols, rank):
-    lan_args = (wd, in_path, out_path, n_rows, n_cols, rank)
+def lanczos_factorize_and_time(project_root, in_path, out_path, n_rows, n_cols, rank):
+    lan_args = (project_root, in_path, out_path, n_rows, n_cols, rank)
     lan_cmd = "{%s/scripts/lanczos-svd.sh %s %s %s %s %s 2> spark.logs; }" % lan_args
     try:
         elapsed_time = time_it(lan_cmd)
@@ -43,8 +43,8 @@ def lanczos_factorize_and_time(wd, in_path, out_path, n_rows, n_cols, rank):
         print "Oops! OS Error. Could not run the command:\n %s" % lan_cmd
     return elapsed_time
 
-def stochastic_factorize_and_time(wd, in_path, out_path, rank):
-    stoch_args = (wd, in_path, out_path, rank)
+def stochastic_factorize_and_time(project_root, in_path, out_path, rank):
+    stoch_args = (project_root, in_path, out_path, rank)
     stoch_cmd = "{%s/scripts/lanczos-svd.sh %s %s %s 2> spark.logs; }" % stoch_args
     try:
         elapsed_time = time_it(stoch_cmd)
@@ -53,7 +53,8 @@ def stochastic_factorize_and_time(wd, in_path, out_path, rank):
     return elapsed_time
 
 
-def process_one_param_set(wd, n_rows, n_cols, frac, lan_rank, stoch_rank, block_size, master, spark_home, project_home, hdfs_root, csv_writer):
+def process_one_param_set(n_rows, n_cols, frac, lan_rank, stoch_rank, block_size, master, spark_home, project_home,
+                          hdfs_root, csv_writer):
     # setup paths for these matrices
     gen_mat_path=hdfs_root + "/gen-matrix-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
     out_u=hdfs_root + "/u-spark-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
@@ -62,12 +63,12 @@ def process_one_param_set(wd, n_rows, n_cols, frac, lan_rank, stoch_rank, block_
     out_lan=hdfs_root + "/lanczos-out-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
     out_stoch=hdfs_root + "/stoch-out-nrow%s-ncols%s-sp%s" % (n_rows, n_cols, frac)
 
-    generate_matrix(wd, gen_mat_path, n_rows, n_cols, frac, block_size, master, spark_home)
-    csv_writer.writerow([spark_factorize_and_time(wd, gen_mat_path, out_u, out_s, out_v, master, spark_home)]
+    generate_matrix(project_home, gen_mat_path, n_rows, n_cols, frac, block_size, master, spark_home)
+    csv_writer.writerow([spark_factorize_and_time(project_home, gen_mat_path, out_u, out_s, out_v, master, spark_home)]
                         + ['spark', n_rows, n_cols, frac, block_size])
-    csv_writer.writerow([lanczos_factorize_and_time(gen_mat_path, out_lan, n_rows, n_cols, lan_rank)]
+    csv_writer.writerow([lanczos_factorize_and_time(project_home, gen_mat_path, out_lan, n_rows, n_cols, lan_rank)]
                         + ['lanczos', n_rows, n_cols, frac])
-    csv_writer.writerow([stochastic_factorize_and_time(wd, gen_mat_path, out_stoch, stoch_rank)]
+    csv_writer.writerow([stochastic_factorize_and_time(project_home, gen_mat_path, out_stoch, stoch_rank)]
                         + ['stoch', n_rows, n_cols, frac])
 
 def main():
@@ -84,16 +85,16 @@ def main():
 
     # Setup our env
     wd = sub.check_output("pwd", shell=True).strip()
-    sub.call(["chmod +x %s/scripts/gen-matrix.sh" % wd], shell=True)
-    sub.call(["chmod +x %s/scripts/spark-svd.sh" % wd], shell=True)
-    sub.call(["chmod +x %s/scripts/lanczos-svd.sh" % wd], shell=True)
-    sub.call(["chmod +x %s/scripts/stochastic-svd.sh" % wd], shell=True)
+    sub.call(["chmod +x %s/scripts/gen-matrix.sh" % project_root], shell=True)
+    sub.call(["chmod +x %s/scripts/spark-svd.sh" % project_root], shell=True)
+    sub.call(["chmod +x %s/scripts/lanczos-svd.sh" % project_root], shell=True)
+    sub.call(["chmod +x %s/scripts/stochastic-svd.sh" % project_root], shell=True)
 
     with open('results/experiment.csv', 'wb') as exp_rez:
         observation_writer = csv.writer(exp_rez, delimiter=",")
         for n_rows in rows:
             for sparse_frac in frac:
-                process_one_param_set(wd, n_rows, n_cols, sparse_frac, lan_rank, stoch_rank, block_size, master,
+                process_one_param_set(n_rows, n_cols, sparse_frac, lan_rank, stoch_rank, block_size, master,
                               spark_home, project_root, hdfs_root, observation_writer)
 
 if __name__ == "__main__":
